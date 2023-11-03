d2f4a21faf387d6d2d73930585e07e02e01f9300
victot-exe <131797231+victot-exe@users.noreply.github.com>
2023-11-03 15:30:34 -0300

    Convenções da linguagem

--- ExemploFundamentos/Models/Pessoa.cs
+++ ExemploFundamentos/Models/Pessoa.cs
@@ -3,14 +3,13 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
-namespace ExemploFundamentos.Models
-{
+namespace ExemploFundamentos.Models{
     public class Pessoa{
-        public string nome { get; set; }
-        public int idade{get; set;}
+        public string Nome {get; set;}
+        public int Idade{get; set;}
 
         public void Apresentar(){
-            Console.WriteLine($"Olá, meu nome é {nome} e eu tenho {idade} anos.");
+            Console.WriteLine($"Olá, meu nome é {Nome} e eu tenho {Idade} anos.");
         }
     }
 }
--- ExemploFundamentos/Program.cs
+++ ExemploFundamentos/Program.cs
@@ -1,2 +1,10 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+// Console.WriteLine("Hello, World!");
+using ExemploFundamentos.Models;
+
+Pessoa p = new(){
+    Nome = "Victor",
+    Idade = 24
+};
+
+p.Apresentar();
