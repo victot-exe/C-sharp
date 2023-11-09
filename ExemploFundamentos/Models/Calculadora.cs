71d62427c550c52c2f85c1a14e3133b92b62b0f9
victot-exe <131797231+victot-exe@users.noreply.github.com>
2023-11-09 15:31:32 -0300

    Add Potencia

--- ExemploFundamentos/Models/Calculadora.cs
+++ ExemploFundamentos/Models/Calculadora.cs
@@ -25,5 +25,10 @@ namespace ExemploFundamentos.Models{
             
             Console.WriteLine($"{x} / {y} = {x / y}");
         }
+
+        public void Potencia(int x, int y){
+            double pot = Math.Pow(x, y);
+            Console.WriteLine($"{x}^{y} = {pot}");
+        }
     }
 }
--- ExemploFundamentos/Program.cs
+++ ExemploFundamentos/Program.cs
@@ -2,7 +2,8 @@ using ExemploFundamentos.Models;
 
 Calculadora calc = new Calculadora();
 
-calc.Somar(1, 2);
-calc.Subtrair(1, 2);
-calc.Multiplicar(2, 2);
-calc.Dividir(10, 3);
+// calc.Somar(1, 2);
+// calc.Subtrair(1, 2);
+// calc.Multiplicar(2, 2);
+// calc.Dividir(10, 3);
+calc.Potencia(3, 3);
