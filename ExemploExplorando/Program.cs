d45e727ca959a73511a4259d929eca3a21d927f4
victot-exe <131797231+victot-exe@users.noreply.github.com>
2023-12-22 16:52:03 -0300

    clear program

--- ExemploExplorando/Program.cs
+++ ExemploExplorando/Program.cs
@@ -1,15 +1,2 @@
 ﻿using ExemploExplorando.Model;
 using ExemploExplorando.Models;
-
-LeituraArquivoTuple arquivo = new();
-
-var (sucesso, linhasArquivo, quantidadeLinhas) = arquivo.LerArquivo("Arquives/arquivoLeiura.txt");
-
-if(sucesso){
-    Console.WriteLine($"Quantidade de linhas do arquivo {quantidadeLinhas}");
-    foreach (string i in linhasArquivo){
-        Console.WriteLine(i);
-    }
-}else{
-    Console.WriteLine("Não foi possível ler o arquivo");
-}
--- ExemploExplorando/obj/Debug/net8.0/ExemploExplorando.AssemblyInfo.cs
+++ ExemploExplorando/obj/Debug/net8.0/ExemploExplorando.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ExemploExplorando")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ce3aa20692ab8451e3380f785e36d8382f3c8509")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1732c25697b7ad6e31828c1192cbe1188da90a46")]
 [assembly: System.Reflection.AssemblyProductAttribute("ExemploExplorando")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ExemploExplorando")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
